8c892110512a4332aafee8ccd5df1c431ec7e983
just <g.staikov85@gmail.com>
2015-12-30 17:25:50 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: just
+ * Date: 30.12.15
+ * Time: 07:46
+ */
+ -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href="resource/css/stylesheet.css" rel='stylesheet' type='text/css'>
+    <link href="resource/css/reset.css" rel='stylesheet' type='text/css'>
+    <title>Web Encounters</title>
+</head>
+<body>
+    <?php require_once "resource/php/header.php";?>
+    <main>
+        <?php require_once "resource/php/welcome.php";?>
+        </main>
+</body>
---
+++ resource/php/header.php
@@ -0,0 +1,34 @@
+<!--
+ * Created by PhpStorm.
+ * User: just
+ * Date: 29.12.15
+ * Time: 18:35
+ -->
+
+<header class="clearFix">
+    <img id="logo" src="resource/img/Logo_150x150.png" alt="Web Encounters">
+    <h1>Web Encounters</h1>
+    <div id="buttons">
+        <div id="lang">
+            <button>Bg</button>
+            <button>En</button>
+        </div>
+        <form action="" method="post" id="login">
+            <span>
+            <label for="userN">User name</label><br>
+            <input id="userN" type="text" name="userN">
+            </span>
+            <span>
+            <label for="pass">Password</label><br>
+             <input id="pass" type="password" name="pass">
+            </span>
+            <span><button type="submit">Go</button> </span>
+    </form>
+        <ul id="fullNav">
+            <li><img src="resource/img/user_icon_small.png"></li>
+            <li><img src="resource/img/menu_icon_small.png"></li>
+            <li><img src="resource/img/heart_small.png"></li>
+            <li><img src="resource/img/message_icon_small.png"></li>
+        </ul>
+    </div>
+</header>
