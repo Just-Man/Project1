6f180587f8cb58aeb0218924de4b0b407732fb0d
just <g.staikov85@gmail.com>
2016-01-02 03:51:10 +0200

    Index Search result

---
+++ indexS.php
@@ -0,0 +1,21 @@
+<!--
+Created by PhpStorm.
+User: just
+Date: 01.01.16
+Time: 09:46
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href="resource/css/stylesheet.css" rel='stylesheet' type='text/css'>
+    <link href="resource/css/reset.css" rel='stylesheet' type='text/css'>
+    <title>Web Encounters</title>
+</head>
+<body>
+    <?php require_once "resource/php/header.php";?>
+<main>
+    <?php require_once "resource/php/searchResult.php";?>
+</main>
+</body>
