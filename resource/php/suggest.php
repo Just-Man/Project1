8bf30006d30f9e92327aabfc80f4eb71e2f27dcf
Aglika <aglika.danailova@gmail.com>
2016-02-05 11:55:22 +0200

    change of colours, fonts, home page outlook

--- resource/php/suggest.php
+++ resource/php/suggest.php
@@ -3,10 +3,14 @@
         <h2>New Profiles</h2>
         <img id="suggLeft" src="resource/img/left_arrow.png">
         <div id="suggContent">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+            <img class="profilePics" src="resource/img/photos/redcarpet1.jpg">
+            <img class="profilePics" src="resource/img/photos/redcarpet2.jpg">
+            <img class="profilePics" src="resource/img/photos/redcarpet3.jpg">
+            <img class="profilePics" src="resource/img/photos/redcarpet4.jpg">
+            <img class="profilePics" src="resource/img/photos/bean1.jpg">
+            <img class="profilePics" src="resource/img/photos/bean2.jpg">
+            <img class="profilePics" src="resource/img/photos/bean3.jpg">
+            <img class="profilePics" src="resource/img/photos/bean4.jpg">
             <span></span>
             <span></span>
             <span></span>
